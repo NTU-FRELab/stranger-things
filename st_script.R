8c5e37b9e859499224dd93c0f118efee37c274d1
THTonyHsu <tonytsaihsuanhsu@gmail.com>
2020-10-14 09:58:12 +0800

    20201014 tidy

--- st_script.R
+++ st_script.R
@@ -14,52 +14,51 @@ rm(list=ls())
 
 ###DATASETS 
 ##raw
-sf=read.table('Data/st_data.txt', sep='\t', header=T)
+sf = read.table('Data/st_data.txt', sep='\t', header=T)
 ##results
-sf.res=sf[,1:3]
+sf.res = sf[,1:3]
 
 ###PREPARATION
 ## ISOTOPES
-sf.res$H15N=sf$H15N
-sf.res$H13C=sf$H13C
-sf.res$A15N=sf$A15N
-sf.res$A13C=sf$A13C
-sf.res$H.C.N=sf$H.C.N
-sf.res$A.C.N=sf$A.C.N
+sf.res$H15N = sf$H15N
+sf.res$H13C = sf$H13C
+sf.res$A15N = sf$A15N
+sf.res$A13C = sf$A13C
+sf.res$H.C.N = sf$H.C.N
+sf.res$A.C.N = sf$A.C.N
 
 ## DW, AFDW, OM AND IM
-#wo:weight_filter,
-#w1:weight_filter_1mL_tissue
-#w2:weight_filter_1mL_tissue_ashed
-#w3:weight_tube
-#w4:weight_tube_1mL_tissue_bleached
-dw=sf$w1-sf$w0 # dry weight tissue in g.mL-1
-afdw=sf$w1-sf$w2 # ash free dry weight tissue in g.mL-1
-dwsc=sf$w4-sf$w3 # dry weight sclerite in g.mL-1
-sf.res$perc_om=afdw/dw*100 # Organic Matter (%)
-sf.res$perc_im=100-sf.res$perc_om # Inorganic Matter (%)
+# wo:weight of filter
+# w1:weight of filter + 1mL tissue
+# w2:weight of filter + 1mL tissue (ashed)
+# w3:weight of tube
+# w4:weight of tube + 1mL tissue (bleached)
+dw = sf$w1-sf$w0 # dry weight tissue in g.mL-1
+afdw = sf$w1-sf$w2 # ash free dry weight tissue in g.mL-1
+dwsc = sf$w4-sf$w3 # dry weight sclerite in g.mL-1
+sf.res$perc_om = afdw/dw*100 # Organic Matter (%)
+sf.res$perc_im = 100-sf.res$perc_om # Inorganic Matter (%)
 
-##ALGAL CELL DENSITY
-#zX_X:cell counting 5 x 1 mm2 repeated five time
-#dil:dilution 
-z1=rowMeans(sf[,c('z1_1','z1_2', 'z1_3','z1_4','z1_5')])
-z2=rowMeans(sf[,c('z2_1','z2_2', 'z2_3','z2_4','z2_5')])
-z3=rowMeans(sf[,c('z3_1','z3_2', 'z3_3','z3_4','z3_5')])
-z4=rowMeans(sf[,c('z4_1','z4_2', 'z4_3','z4_4','z4_5')])
-z5=rowMeans(sf[,c('z5_1','z5_2', 'z5_3','z5_4','z5_5')])
-count=(z1+z2+z3+z4+z5)*10000/(5*(1/sf$dil)) # Algal cell in 1 mL 
-sf.res$cell.afdw=count/afdw # algal cell.gafdw-1 
+## ALGAL CELL DENSITY
+# zX_X:cell counting 5 x 1 mm2 repeated five time
+# dil:dilution 
+z1 = rowMeans(sf[,c('z1_1','z1_2', 'z1_3','z1_4','z1_5')])
+z2 = rowMeans(sf[,c('z2_1','z2_2', 'z2_3','z2_4','z2_5')])
+z3 = rowMeans(sf[,c('z3_1','z3_2', 'z3_3','z3_4','z3_5')])
+z4 = rowMeans(sf[,c('z4_1','z4_2', 'z4_3','z4_4','z4_5')])
+z5 = rowMeans(sf[,c('z5_1','z5_2', 'z5_3','z5_4','z5_5')])
+count = (z1+z2+z3+z4+z5)*10000/(5*(1/sf$dil)) # Algal cell in 1 mL 
+sf.res$cell.afdw = count/afdw # algal cell.gafdw-1 
 
 
-##CHLOROPHYLLS
-#
-#
-Ca=11.85*(sf$A2-sf$A1)-1.54*(sf$A3-sf$A1)-0.08*(sf$A4-sf$A1)
-Cb=-5.43*(sf$A2-sf$A1)+21.03*(sf$A3-sf$A1)-2.66*(sf$A4-sf$A1)
-Cc=-1.67*(sf$A2-sf$A1)-7.6*(sf$A3-sf$A1)+24.52*(sf$A4-sf$A1)
-sf.res$chla.afdw=Ca*sf$V_extra/afdw  # ug chl / gafdw 
-sf.res$chlb.afdw=Cb*sf$V_extra/afdw  # ug chl / gafdw 
-sf.res$chlc.afdw=Cc*sf$V_extra/afdw  # ug chl / gafdw 
+## CHLOROPHYLLS
+
+Ca = 11.85*(sf$A2-sf$A1)-1.54*(sf$A3-sf$A1)-0.08*(sf$A4-sf$A1)
+Cb = -5.43*(sf$A2-sf$A1)+21.03*(sf$A3-sf$A1)-2.66*(sf$A4-sf$A1)
+Cc = -1.67*(sf$A2-sf$A1)-7.6*(sf$A3-sf$A1)+24.52*(sf$A4-sf$A1)
+sf.res$chla.afdw = Ca*sf$V_extra/afdw  # ug chl / gafdw 
+sf.res$chlb.afdw = Cb*sf$V_extra/afdw  # ug chl / gafdw 
+sf.res$chlc.afdw = Cc*sf$V_extra/afdw  # ug chl / gafdw 
 # sf.res$chlac.afdw = sf.res$chla.afdw + sf.res$chlc.afdw
 # sf.res$chla.cell = sf.res$chla.afdw/sf.res$cell.afdw
 
